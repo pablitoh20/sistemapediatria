3d83a48c8a407f7ac2e7b93cac2c41a0951877f5
pablo <pablitoherrera29@gmail.com>
2019-12-03 00:37:34 -0300

    lalala

--- conexion.php
+++ conexion.php
@@ -1,13 +1,13 @@
 <?php
 
-$host='127.0.0.1';
-$user='root';
-$pass='';
-$db='SistemaExamenesPediatria';
+$host='ec2-23-21-249-0.compute-1.amazonaws.com';
+$user='pmldzicfqdwusr';
+$pass='768252c37cf78693b3479470bd3c6f4d266e41e9a82ff7399c9fbcdfa8287457';
+$db='d60qt4si56m7uq';
 
 $con=mysqli_connect($host,$user,$pass,$db) or die();
 
- 
+
 
 /*para conexion servidor */
 //$con=mysqli_connect("localhost","bienestar","kxrbnWzq1ds9Sa","bienestar") or die("No se pudo conectar con el servidor");?>

00caa3f955b29638ea6c5555e3ac87ae032ced91
pablo <pablitoherrera29@gmail.com>
2019-12-03 00:49:58 -0300

    a

--- conexion.php
+++ conexion.php
@@ -1,9 +1,9 @@
 <?php
 
-$host='ec2-23-21-249-0.compute-1.amazonaws.com';
-$user='pmldzicfqdwusr';
-$pass='768252c37cf78693b3479470bd3c6f4d266e41e9a82ff7399c9fbcdfa8287457';
-$db='d60qt4si56m7uq';
+$host='ec2-174-129-253-27.compute-1.amazonaws.com';
+$user='xscqjrpsobfewa';
+$pass='b6a846234b9c08a39cd8dda7a751bdb24e719e9d69706e7159964a3c37f58fcd';
+$db='d6i0htst325322';
 
 $con=mysqli_connect($host,$user,$pass,$db) or die();
 

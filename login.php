e1a8a48ba3f55b58bc750dce7f45bc66da88fba5
pablo <pablitoherrera29@gmail.com>
2019-12-10 18:37:27 -0300

    test

--- login.php
+++ login.php
@@ -1,6 +1,9 @@
 <?php
-require_once($_SERVER["DOCUMENT_ROOT"].'/sistemapediatria/funciones.php');
-require_once($_SERVER["DOCUMENT_ROOT"].'/sistemapediatria/header.php');
+if (getenv('MY_VAR') == 1) {
+  require_once($_SERVER["DOCUMENT_ROOT"].'/app/sistemapediatria/funciones.php');
+  require_once($_SERVER["DOCUMENT_ROOT"].'/app/sistemapediatria/header.php');
+}
+
 if (isset($_POST['login'])) {
   login($_POST);
 }

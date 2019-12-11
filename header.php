ed8458ab225e9e8dc11e7ddaba50ab75aed7d44b
pablo <pablitoherrera29@gmail.com>
2019-12-11 18:59:20 -0300

    a

--- header.php
+++ header.php
@@ -12,14 +12,14 @@
   <title>Sistema Examen Pediatria</title>
 
   <!-- Custom fonts for this template-->
-  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="sistemapeadiatria/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
   <script src="js/admin.js"></script>
   <!-- Custom styles for this template-->
-  <link href="css/admin.css" rel="stylesheet">
+  <link href="sistemapeadiatria/css/admin.css" rel="stylesheet">
 
-  <link href="css/sb-admin-2.min.css" rel="stylesheet">
+  <link href="sistemapeadiatria/css/sb-admin-2.min.css" rel="stylesheet">
 
   <script type='text/javascript' src='https://cdn.jsdelivr.net/npm/froala-editor@3.0.6/js/froala_editor.pkgd.min.js'></script>
   <link href='https://cdn.jsdelivr.net/npm/froala-editor@3.0.6/css/froala_editor.pkgd.min.css' rel='stylesheet' type='text/css' />

078df729edf03bf3c1aeca6311169a296cda3873
pablo <pablitoherrera29@gmail.com>
2019-12-11 01:16:06 -0300

    a

--- conexion.php
+++ conexion.php
@@ -1,4 +1,5 @@
 <?php
+$con = pg_connect("host=ec2-174-129-254-217.compute-1.amazonaws.com dbname=d2rhleqfkoibp user=mnebvrgxcnyoqs password=635159d96965b30d026ab51bb6b5e7d68ac38817df4de895a45804659f849708");
 
 /*$host='127.0.0.1';
 $user='root';
@@ -7,7 +8,6 @@ $db='SistemaExamenesPediatria';
 
 $con=mysqli_connect($host,$user,$pass,$db) or die();
 */
-$con = pg_connect("host=ec2-174-129-254-217.compute-1.amazonaws.com dbname=d2rhleqfkoibp user=mnebvrgxcnyoqs password=635159d96965b30d026ab51bb6b5e7d68ac38817df4de895a45804659f849708");
 
 
 /*para conexion servidor */

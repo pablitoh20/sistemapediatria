f8e482ed27f00515014f0a4a8bf2cb147b76e045
pablo <pablitoherrera29@gmail.com>
2019-12-11 19:39:12 -0300

    a

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
   <title>Sistema Examen Pediatria</title>
 
   <!-- Custom fonts for this template-->
-  <link href="./vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="app/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
   <script src="js/admin.js"></script>

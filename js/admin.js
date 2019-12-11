8cacf132c8a6b5f5bdf84025413d89f51c4a9548
pablo <pablitoherrera29@gmail.com>
2019-12-11 19:52:51 -0300

    ad

--- js/admin.js
+++ js/admin.js
@@ -2,19 +2,19 @@ $(document).ready(function(){
   $('.seleccionar').click(function(){
     document.getElementById("seleccionadas").textContent=$(".seleccionar:checked").length
   });
-  
+
 
   $("#descarga_aleatorea").click(function(){
 
     var data = 'cantidad=' + $('#cant_pregs').val() + "&curso="+$("#curso").val();
-      $.ajax({type:'POST',data:data,url: './ajax/ajax_descarga_aleatorea.php',	success: function(result){
+      $.ajax({type:'POST',data:data,url: '/ajax/ajax_descarga_aleatorea.php',	success: function(result){
       }});
   });
 });
 function eliminarPregunta(id){
   if (confirm("Desea eliminar la pregunta?")) {
       var data = 'id=' + id;
-  			$.ajax({type:'POST',data:data,url: './ajax/ajax_delete.php',	success: function(result){
+  			$.ajax({type:'POST',data:data,url: '/ajax/ajax_delete.php',	success: function(result){
              $('.notification').slideDown('slow');
              window.setTimeout(close1,2500);
               $('tr[data-name='+id+']').remove();
